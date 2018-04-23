bb3a4797402c4e8be26e4bd5d34a612de6a6fda5
anceh <ancehx@gmail.com>
2018-04-23 14:43:12 +0200

    scene

---
+++ Platform Tutorial/Assets/Scripts/Enviroment/Spikes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour {
+
+
+    private float damage = 1.0f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerVariables>().Harm();
+        }
+    }
+
+
+
+
+    void Start ()
+    {
+		
+	}
+
+
+    void Update ()
+    {
+		
+	}
+}
