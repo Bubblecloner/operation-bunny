a01b8c5d87a853f760deb0bd2d7ec4d70ee7007a
anceh <ancehx@gmail.com>
2018-05-04 11:52:17 +0200

    upd skybox + camera

---
+++ Platform Tutorial/Assets/Scripts/Skyboxrotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skyboxrotate : MonoBehaviour {
+
+    private Skybox sky;
+
+	void Start ()
+    {
+        sky = GetComponent<Skybox>();
+	}
+	
+	void Update ()
+    {
+        sky.material.SetFloat("_Rotation", Time.time);
+	}
+}
