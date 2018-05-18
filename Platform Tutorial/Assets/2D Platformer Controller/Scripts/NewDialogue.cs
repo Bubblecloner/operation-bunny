e9ca3e132f981623f17cf68eadcaa4d71b349863
anceh <ancehx@gmail.com>
2018-05-18 16:04:11 +0200

    dialog + death's voice

---
+++ Platform Tutorial/Assets/2D Platformer Controller/Scripts/NewDialogue.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class NewDialogue : MonoBehaviour {
+
+    public string firstParagraph, secondParagraph, thirdParagraph;
+    public Canvas canvas;
+    public Text textObject;
+    public Text leftAnswer, rightAnswer;
+
+    private float readTime = 3.0f;
+    private bool waitingForAnswer;
+
+	void Start ()
+    {
+        canvas.enabled = false;
+        leftAnswer.enabled = false;
+        rightAnswer.enabled = false;
+        waitingForAnswer = false;
+	}
+
+    private void Update()
+    {
+        // Testar metoden
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            canvas.enabled = true;
+            PrintText(firstParagraph);
+        }
+
+        // Får svar från spelaren
+        if(waitingForAnswer && Input.GetKeyDown(KeyCode.A))
+        {
+            DisableAnswers();
+            PrintText(secondParagraph);
+        }
+
+        if (waitingForAnswer && Input.GetKeyDown(KeyCode.B))
+        {
+            DisableAnswers();
+            PrintText(thirdParagraph);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Stoppa spelaren
+        canvas.enabled = true;
+        PrintText(firstParagraph);
+    }
+
+    private void PrintText(string textToPrint)
+    {
+        textObject.text = "";
+        textObject.DOText(textToPrint, readTime);
+        Invoke("EnableAnswers", readTime + 1.0f);
+    }
+
+    private void EnableAnswers()
+    {
+        leftAnswer.enabled = true;
+        rightAnswer.enabled = true;
+        waitingForAnswer = true;
+    }
+
+    private void DisableAnswers()
+    {
+        leftAnswer.enabled = false;
+        rightAnswer.enabled = false;
+        waitingForAnswer = false;
+    }
+}
