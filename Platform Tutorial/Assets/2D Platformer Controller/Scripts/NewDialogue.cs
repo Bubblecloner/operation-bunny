711d2e383f0f6b6d15a19b7c3859645fea8d12f8
MartyMcFlew <invictus83@gmail.com>
2018-05-22 12:01:19 +0200

    More dialogue

--- Platform Tutorial/Assets/2D Platformer Controller/Scripts/NewDialogue.cs
+++ Platform Tutorial/Assets/2D Platformer Controller/Scripts/NewDialogue.cs
@@ -6,13 +6,14 @@ using DG.Tweening;
 
 public class NewDialogue : MonoBehaviour {
 
-    public string firstParagraph, secondParagraph, thirdParagraph;
+    public string firstParagraph, secondParagraph, thirdParagraph, fourthParagraph, fifthParagraph, sixthParagraph, seventhParagraph, eightParagraph, ninthParagraph, tenthParagraph;
     public Canvas canvas;
     public Text textObject;
     public Text leftAnswer, rightAnswer;
 
     private float readTime = 3.0f;
     private bool waitingForAnswer;
+    private int numberOfNos;
 
 	void Start ()
     {
@@ -20,6 +21,7 @@ public class NewDialogue : MonoBehaviour {
         leftAnswer.enabled = false;
         rightAnswer.enabled = false;
         waitingForAnswer = false;
+        numberOfNos = 0;
 	}
 
     private void Update()
@@ -40,9 +42,97 @@ public class NewDialogue : MonoBehaviour {
 
         if (waitingForAnswer && Input.GetKeyDown(KeyCode.B))
         {
+            numberOfNos++;
             DisableAnswers();
-            PrintText(thirdParagraph);
+
+            switch (numberOfNos)
+            {
+                case 1:
+                    PrintText(thirdParagraph);
+                    break;
+                case 2:
+                    PrintText(fourthParagraph);
+                    break;
+                case 3:
+                    PrintText(fifthParagraph);
+                    break;
+                case 4:
+                    PrintText(sixthParagraph);
+                    break;
+                case 5:
+                    PrintText(seventhParagraph);
+                    break;
+                case 6:
+                    PrintText(eightParagraph);
+                    break;
+                case 7:
+                    PrintText(ninthParagraph);
+                    break;
+                case 8:
+                    PrintText(tenthParagraph);
+                    break;
+                case 9:
+                    GetGary();
+                    break;
+                default:
+                    Debug.Log("Switch broke");
+                    break;
+            }
         }
+    }
+
+    private void GetGary()
+    {
+        //Animera gary och vänta, gå sen igenom dialogen (ändra 1.0f till tiden det tar för animationen)
+        StartCoroutine("GaryDialogue", 1.0f);
+    }
+
+    private IEnumerator GaryDialogue()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            switch (i)
+            {
+                case 0:
+                    PrintText("Aaaaaaaaaargh… Gary!");
+                    yield return new WaitForSeconds(4.0f);
+                    break;
+                case 1:
+                    PrintText("Yes, Grand master ?");
+                    yield return new WaitForSeconds(4.0f);
+                    break;
+                case 2:
+                    PrintText("Go find the bunny and let her kill you");
+                    yield return new WaitForSeconds(4.0f);
+                    break;
+                case 3:
+                    PrintText("Right, go kill the bunny.");
+                    yield return new WaitForSeconds(4.0f);
+                    break;
+                case 4:
+                    PrintText("No! Let her kill you!");
+                    yield return new WaitForSeconds(4.0f);
+                    break;
+                case 5:
+                    PrintText("Go get killed by the bunny. Right, got it.");
+                    yield return new WaitForSeconds(4.0f);
+                    break;
+                case 6:
+                    PrintText("You understand, Gary?");
+                    yield return new WaitForSeconds(4.0f);
+                    break;
+                case 7:
+                    PrintText("Yes, grand master!");
+                    yield return new WaitForSeconds(4.0f);
+                    // Animera Gary en gång till här, och sen en till switch när han kommer tillbaka
+                    break;
+                default:
+                    Debug.Log("Switch 2 broke");
+                    break;
+            }
+        }
+
+        yield break;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
