1ffe1a6884a72f3103c80b0741e9b3fe0d463129
MartyMcFlew <invictus83@gmail.com>
2018-05-28 13:26:01 +0200

    Scene 04 start

---
+++ Platform Tutorial/Assets/Scripts/Scene04/AnimatedUVs.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatedUVs : MonoBehaviour
+{
+    public int materialIndex = 0;
+    public Vector2 uvAnimationRate = new Vector2(1.0f, 0.0f);
+    public string textureName = "_MainTex";
+
+    Vector2 uvOffset = Vector2.zero;
+    private MeshRenderer renderer;
+
+    private void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+
+    void LateUpdate()
+    {
+        uvOffset += (uvAnimationRate * Time.deltaTime);
+        if (renderer.enabled)
+        {
+            renderer.materials[materialIndex].SetTextureOffset(textureName, uvOffset);
+        }
+    }
+}
---
+++ Platform Tutorial/Assets/Scripts/Scene04/Scene04Lift.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Scene04Lift : MonoBehaviour {
+
+    private Rigidbody2D rgbd;
+    private bool moving;
+
+    private void Start()
+    {
+        rgbd = GetComponent<Rigidbody2D>();
+        moving = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (moving)
+        {
+            rgbd.MovePosition(transform.position + new Vector3(3.0f, 0, 0) * Time.fixedDeltaTime);
+        }
+    }
+
+    public void Drop()
+    {
+        rgbd.DOMove(new Vector2(transform.position.x, -4), 1.0f);
+        Invoke("Move", 2.0f);
+    }
+
+    private void Move()
+    {
+        moving = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Player"))
+        {
+            other.transform.parent = transform;
+        }
+
+        if (other.collider.CompareTag("Stop"))
+        {
+            moving = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+        }
+    }
+}
