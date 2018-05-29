334ec61512c0be248397f84783da7d7a3bde89fa
MartyMcFlew <invictus83@gmail.com>
2018-05-29 11:47:52 +0200

    Updated scene 04

--- Platform Tutorial/Assets/Scripts/Scene04/Scene04Lift.cs
+++ Platform Tutorial/Assets/Scripts/Scene04/Scene04Lift.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class Scene04Lift : MonoBehaviour {
 
+    public Image arrow;
+
     private Rigidbody2D rgbd;
     private bool moving;
+    private Transform playerPosition;
 
     private void Start()
     {
         rgbd = GetComponent<Rigidbody2D>();
         moving = false;
+        playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
+        arrow.enabled = false;
+        FadeArrowOut();
     }
 
     private void FixedUpdate()
@@ -20,17 +27,32 @@ public class Scene04Lift : MonoBehaviour {
         {
             rgbd.MovePosition(transform.position + new Vector3(3.0f, 0, 0) * Time.fixedDeltaTime);
         }
+
+        arrow.transform.position = new Vector3(transform.position.x, playerPosition.position.y + 5, transform.position.z);
     }
 
     public void Drop()
     {
         rgbd.DOMove(new Vector2(transform.position.x, -4), 1.0f);
         Invoke("Move", 2.0f);
+        arrow.enabled = true;
     }
 
     private void Move()
     {
         moving = true;
+    }
+
+    private void FadeArrowIn()
+    {
+        arrow.DOFade(0.8f, 1.0f);
+        Invoke("FadeArrowOut", 1.0f);
+    }
+
+    private void FadeArrowOut()
+    {
+        arrow.DOFade(0.4f, 1.0f);
+        Invoke("FadeArrowIn", 1.0f);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
