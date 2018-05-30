1749d61c0952c22852acaf2cd92dda5dc037978f
anceh <ancehx@gmail.com>
2018-05-30 09:53:27 +0200

    fadeinout

---
+++ Platform Tutorial/Assets/Scripts/Levelchanger.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public class Levelchanger : MonoBehaviour {
+
+    public Animator animator;
+
+	void Update ()
+
+        private void OnTriggerEnter2D(Collider2D exit)
+
+    {
+        FadeToLevel(6);    
+    }
+  
+
+    public void FadeToLevel (int levelIndex)
+    {
+        animator.SetTrigger("blackfadeout");
+    }
+}
